86e60d94afc1cf85512eeb0020d6ccfc4462e227
ostapula <ostapnote2@gmail.com>
2023-08-08 17:40:36 +0200

    #3 Added new unit tests and refactor algorithm implementation to use recursion

--- src/main/java/com/ostapula/maven/classes/App.java
+++ src/main/java/com/ostapula/maven/classes/App.java
@@ -22,48 +22,57 @@ public class App
 	public static List<Integer> sortIntegers(List<Integer> xs) {
 		List<Integer> copyXs = new ArrayList<Integer>(xs);
     	int listSize = copyXs.size();
-
-        for (int currSize = 1; currSize < listSize; currSize *= 2) {
-            for (int left = 0; left < listSize; left += currSize * 2) { 
-            	
-            	int mid = left + currSize;
-            	if (mid >= listSize) {
-            		mid = listSize - 1;
-            	}
-            	
-            	int right = left + currSize * 2;
-            	if (right >= listSize) {
-            		right = listSize;
-            	} 
-
-                List<Integer> leftList = new ArrayList<Integer>(copyXs.subList(left, mid));
-                List<Integer> rightList = new ArrayList<Integer>(copyXs.subList(mid, right));
-
-                int i = 0, j = 0, k = left;
-                while (i < leftList.size() && j < rightList.size()) {
-                    if (leftList.get(i) <= rightList.get(j)) {
-                    	copyXs.set(k, leftList.get(i));
-                        i++;
-                    } else {
-                    	copyXs.set(k, rightList.get(j));
-                        j++;
-                    }
-                    k++;
-                }
-
-                while (i < leftList.size()) {
-                	copyXs.set(k, leftList.get(i));
-                    i++;
-                    k++;
-                }
-
-                while (j < rightList.size()) {
-                	copyXs.set(k, rightList.get(j));
-                    j++;
-                    k++;
-                }
-            }
-        }
+    	if (copyXs.isEmpty()) {
+    		return copyXs;
+    	}
+    	int currSize = 1, left = 0;
+    	merge(copyXs, listSize ,currSize, left);
         return copyXs;
 	}
+	
+	public static void merge(List<Integer> copyXs, int listSize, int currSize, int left) {
+        System.out.println("currSize " + currSize + " | left " + left);
+		int mid = left + currSize;
+        if (mid >= listSize) {
+            mid = listSize - 1;
+        }
+            	
+        int right = left + currSize * 2;
+        if (right >= listSize) {
+        	right = listSize;
+        } 
+
+        List<Integer> leftList = new ArrayList<Integer>(copyXs.subList(left, mid));
+        List<Integer> rightList = new ArrayList<Integer>(copyXs.subList(mid, right));
+
+        int i = 0, j = 0, k = left;
+        while (i < leftList.size() && j < rightList.size()) {
+        	if (leftList.get(i) <= rightList.get(j)) {
+        		copyXs.set(k, leftList.get(i));
+        		i++;
+        	} else {
+            	copyXs.set(k, rightList.get(j));
+                j++;
+        	}
+            k++;
+        }
+
+        while (i < leftList.size()) {
+        	copyXs.set(k, leftList.get(i));
+            i++;
+            k++;
+        }
+
+        while (j < rightList.size()) {
+        	copyXs.set(k, rightList.get(j));
+            j++;
+            k++;
+        }
+        left += currSize * 2;
+		if (left < listSize) {
+			merge(copyXs, listSize, currSize, left);
+		} else if (left >= listSize && currSize <= listSize) {
+			merge(copyXs, listSize, currSize *= 2, 0);
+		}
+	}     	
 }
