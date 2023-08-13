21f417b6f900d909875a11871eaf382fa2064d1b
ostapula <ostapnote2@gmail.com>
2023-08-14 01:00:28 +0200

    #3 Re-wrote the algorithm using tail recursion and added appropriate tests to unit test

--- src/main/java/com/ostapula/maven/classes/App.java
+++ src/main/java/com/ostapula/maven/classes/App.java
@@ -4,46 +4,76 @@ import java.util.*;
 
 public class App 
 {
-	
+	public static int counter = 0;
     public static void main( String[] args )
     {
     	List<Integer> xs = new ArrayList<Integer>();
-    	generateInteger(xs);
+    	generateInteger(xs); 
     	System.out.println("Not sorted list " + xs + ". Sorted list " + sortIntegers(xs));
     }
     
     public static void generateInteger(List<Integer> xs) {
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
-            xs.add(random.nextInt(1, 100)); 
+        	xs.add(random.nextInt(1 ,100));
         }
     }
 	
 	public static List<Integer> sortIntegers(List<Integer> xs) {
-		List<Integer> copyXs = new ArrayList<Integer>(xs);
-    	int listSize = copyXs.size();
-    	if (copyXs.isEmpty()) {
-    		return copyXs;
+    	if (xs.isEmpty()) {
+    		throw new IllegalArgumentException("The list cannot be empty!");
+    	} 
+    	List<Integer> copyXs = new ArrayList<Integer>(xs);
+    	if (copyXs.contains(null)) {
+    		for (int i = 0; i < copyXs.size(); i++) {
+    			if(copyXs.get(i) == null) {
+    				copyXs.remove(i);
+    				counter++;
+    				i--;
+    			}
+    		}
     	}
-    	int currSize = 1, left = 0;
-    	merge(copyXs, listSize ,currSize, left);
+    	
+    	int listSize = copyXs.size();
+    	int left = 0;
+    	mergeSort(copyXs, listSize, left);
+    	for (int i = 0; i < counter; i++) {
+    		copyXs.add(i, null);
+    	}
         return copyXs;
 	}
 	
-	public static void merge(List<Integer> copyXs, int listSize, int currSize, int left) {
-		int mid = left + currSize;
-        if (mid >= listSize) {
-            mid = listSize - 1;
-        }
-            	
-        int right = left + currSize * 2;
-        if (right >= listSize) {
-        	right = listSize;
-        } 
-
-        List<Integer> leftList = new ArrayList<Integer>(copyXs.subList(left, mid));
+	public static int mergeSort(List<Integer> copyXs, int listSize, int left) {
+		return mergeSort(copyXs, listSize, left, 0, 0, 1);
+	}
+	
+	public static int mergeSort(List<Integer> copyXs, int listSize, int left, int mid, int right, int currSize) {
+		if (currSize >= listSize) {
+			return 1;
+		}
+		if (left >= listSize) {
+			return mergeSort(copyXs, listSize, 0, 0, 0, currSize *= 2);
+		}
+    	mid = left + currSize;
+    	if (mid >= listSize) {
+    		mid = listSize - 1;
+    	}
+    	
+    	right = left + currSize * 2;
+    	if (right >= listSize) {
+    		right = listSize;
+    	} 
+    	
+		merge(copyXs, left, mid, right);
+		
+		return mergeSort(copyXs, listSize, left += currSize * 2, 0, 0, currSize);
+	}
+	
+	public static void merge(List<Integer> copyXs, int left, int mid, int right) {
+		
+		List<Integer> leftList = new ArrayList<Integer>(copyXs.subList(left, mid));
         List<Integer> rightList = new ArrayList<Integer>(copyXs.subList(mid, right));
-
+			
         int i = 0, j = 0, k = left;
         while (i < leftList.size() && j < rightList.size()) {
         	if (leftList.get(i) <= rightList.get(j)) {
@@ -67,11 +97,5 @@ public class App
             j++;
             k++;
         }
-        left += currSize * 2;
-		if (left < listSize) {
-			merge(copyXs, listSize, currSize, left);
-		} else if (left >= listSize && currSize <= listSize) {
-			merge(copyXs, listSize, currSize *= 2, 0);
-		}
-	}     	
+	}
 }
--- src/test/java/com/ostapula/maven/classes/AppTest.java
+++ src/test/java/com/ostapula/maven/classes/AppTest.java
@@ -4,10 +4,11 @@ import static org.junit.Assert.*;
 import java.util.*;
 import org.junit.Test;
 
+
 public class AppTest 
 {   
     @Test
-    public void testTwoListsOfIntegersAreEqual()
+    public void testUnorderedListBecomesOrdered()
     {
     	List<Integer> inputList = Arrays.asList(23, 12, 1, 5, 87, 56, 34, 9, 20, 8);
     	List<Integer> expectedList = new ArrayList<Integer>(inputList);
@@ -16,7 +17,7 @@ public class AppTest
     }
     
     @Test
-    public void testSortSortedListOfIntegers()
+    public void testSortingOrderedListRemainsOrdered()
     {
     	List<Integer> inputList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     	List<Integer> expectedList = new ArrayList<Integer>(inputList);
@@ -25,58 +26,38 @@ public class AppTest
     }
     
     @Test
-    public void testSortTwoIntegerNumbers()
+    public void testListContentRemainsUnchagedAfterSorting()
     {
-    	List<Integer> inputList = Arrays.asList(2, 1);
-    	List<Integer> expectedList = new ArrayList<Integer>(inputList);
-    	expectedList.sort(null);    	
-    	assertEquals(expectedList, App.sortIntegers(inputList));
-    }
-    
-    @Test
-    public void testSortTwoEqualIntegerNumbers()
-    {
-    	List<Integer> inputList = Arrays.asList(2, 2);
-    	List<Integer> expectedList = new ArrayList<Integer>(inputList);
-    	expectedList.sort(null);    	
-    	assertEquals(expectedList, App.sortIntegers(inputList));
-    }
-    
-    @Test
-    public void testSortOneIntegerNumber()
-    {
-    	List<Integer> inputList = Arrays.asList(0);
-    	List<Integer> expectedList = new ArrayList<Integer>(inputList);
+    	List<Integer> inputList = Arrays.asList(23, 12, 1, 5, 87, 56, 34, 9, 20, 8);	
+    	List<Integer> expectedList = new ArrayList<Integer>(inputList); 	
+    	List<Integer> copyOfInputList = new ArrayList<Integer>(inputList);  	
     	expectedList.sort(null);    		
-    	assertEquals(expectedList, App.sortIntegers(inputList));
+    	assertEquals(copyOfInputList, inputList);
+    }
+    
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSortingEmptyListThrowsException() {
+        List<Integer> inputList = new ArrayList<Integer>();
+        App.sortIntegers(inputList);        
     }
     
     @Test
-    public void testSortEmptyList()
-    {
-    	List<Integer> inputList = new ArrayList<Integer>();
-    	List<Integer> expectedList = new ArrayList<Integer>(inputList);
-    	expectedList.sort(null);    	
-    	assertEquals(expectedList, App.sortIntegers(inputList));
-    }
-    
-    @Test
-    public void  testGenerateIntegerListSize () {
-    	List<Integer> generateList = new ArrayList<Integer>();
-    	
-    	App.generateInteger(generateList);
-    	
-    	assertEquals(10, generateList.size());
-    }
-    
-    @Test
-    public void testGenerateIntegerValues() {
-    	List<Integer> generateList = new ArrayList<Integer>();
-    	
-    	App.generateInteger(generateList);
-    	
-    	for (int a : generateList) {
-    		assertEquals(true, 0 < a && a < 100);
+    public void testNullElementsAreSmallerThanAnyInteger() {
+    	List<Integer> inputList = Arrays.asList(1, 12, 5, null, 87, 56, 34, null, 20, 8);
+    	List<Integer> expectedList = new ArrayList<Integer>(inputList); 	
+		int counter = 0;
+    	for (int i = 0; i < expectedList.size(); i++) {
+			if(expectedList.get(i) == null) {
+				expectedList.remove(i);
+				counter++;
+				i--;
+			}
     	}
+    	expectedList.sort(null);
+    	for (int i = 0; i < counter; i++) {
+    		expectedList.add(i, null);
+    	}
+    	assertEquals(expectedList, App.sortIntegers(inputList));   		
     }
 }
