d99acfd367d924221b158a62cb9fb06f0b6e2210
ostapula <ostapnote2@gmail.com>
2023-08-22 23:30:31 +0200

    #6 Improved code formatting, made some methods private, decoupled factorial functions from comparison app.

---
+++ src/main/java/com/ostapula/factorial/FactorialCalculator.java
@@ -0,0 +1,23 @@
+package com.ostapula.factorial;
+
+public class FactorialCalculator {
+	
+	public int factorialNonTailRecursion(int n) {
+    	if (n == 1) {
+    		return n;
+    	}
+    	int result = factorialNonTailRecursion(n - 1);
+    	return n * result;
+    }
+    
+	public int factorialTailRecursion(int n) {
+    	return factorialTailRecursion(n, 1);
+    }
+    
+    private int factorialTailRecursion(int n, int result) {
+    	if (n == 0) {
+    		return result;
+    	}
+    	return factorialTailRecursion(n - 1, result * n);
+    }
+}
--- src/main/java/com/ostapula/recursioncomparison/RecursionComparison.java
+++ src/main/java/com/ostapula/recursioncomparison/RecursionComparison.java
@@ -1,63 +1,43 @@
 package com.ostapula.recursioncomparison;
 
-public class RecursionComparison 
-{
+import com.ostapula.factorial.FactorialCalculator;
+
+public class RecursionComparison {
+ 
 	private RecursionComparison() {
 		throw new AssertionError();
 	}
-	
-    public static void main( String[] args )
-    {
-    	int nonTailMax = maxIntNonTailRecursion();
-    	int tailMax = maxIntTailRecursion();
-    	
+
+	public static void main(String[] args) {
+		FactorialCalculator factorialCalculator = new FactorialCalculator();
+		
+		final int nonTailMax = maxIntNonTailRecursion(1, factorialCalculator);
+    	final int tailMax = maxIntTailRecursion(1, factorialCalculator);
+
     	if (nonTailMax >= tailMax) {
     		System.out.println("The function IS NOT benefiting from tail recursion: non-tail recursion: " + nonTailMax + " >= tail recursion: " + tailMax);
     	} else {
     		System.out.println("The function IS benefiting from tail recursion: non-tail recursion: " + nonTailMax + " < tail recursion: " + tailMax);
     	}   	
     }
-    
-    public static int maxIntNonTailRecursion() {
-    	int n = 1;
+
+    private static int maxIntNonTailRecursion(int n, FactorialCalculator factorialCalculator) {
     	while(true) {
     		try {
-    			factorialNonTailRecursion(n);
-    			n++;
+    			factorialCalculator.factorialNonTailRecursion(n++);
     		} catch (StackOverflowError e) {
     			return n - 1;
     		}
     	}
     }
-    
-    public static int maxIntTailRecursion() {
-    	int n = 1;
+
+    private static int maxIntTailRecursion(int n, FactorialCalculator factorialCalculator) {
     	while(true) {
-    		try {
-    			factorialTailRecursion(n);
-    			n++;
+    		try {    			
+				factorialCalculator.factorialTailRecursion(n++);
     		} catch (StackOverflowError e) {
     			return n - 1;
     		}
     	}
-    }
-    
-    public static int factorialNonTailRecursion(int n) {
-    	if (n == 1) {
-    		return n;
-    	}
-    	int sum = factorialNonTailRecursion(n - 1);
-    	return n *= sum;
-    }
-    
-    public static int factorialTailRecursion(int n) {
-    	return factorialTailRecursion(n, 1);
-    }
-    
-    public static int factorialTailRecursion(int n, int sum) {
-    	if (n == 0) {
-    		return sum;
-    	}
-    	return factorialTailRecursion(n - 1, sum *= n);
     }
 }
