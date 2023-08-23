604e3c047eac988fc339e028778bf37176ce8ac4
ostapula <ostapnote2@gmail.com>
2023-08-24 00:28:40 +0200

    #6 Improved code formatting

--- src/main/java/com/ostapula/recursioncomparison/RecursionComparison.java
+++ src/main/java/com/ostapula/recursioncomparison/RecursionComparison.java
@@ -12,32 +12,32 @@ public class RecursionComparison {
 		FactorialCalculator factorialCalculator = new FactorialCalculator();
 		
 		final int nonTailMax = maxIntNonTailRecursion(1, factorialCalculator);
-    	final int tailMax = maxIntTailRecursion(1, factorialCalculator);
+		final int tailMax = maxIntTailRecursion(1, factorialCalculator);
 
-    	if (nonTailMax >= tailMax) {
-    		System.out.println("The function IS NOT benefiting from tail recursion: non-tail recursion: " + nonTailMax + " >= tail recursion: " + tailMax);
-    	} else {
-    		System.out.println("The function IS benefiting from tail recursion: non-tail recursion: " + nonTailMax + " < tail recursion: " + tailMax);
-    	}   	
-    }
+		if (nonTailMax >= tailMax) {
+			System.out.println("The function IS NOT benefiting from tail recursion: non-tail recursion: " + nonTailMax + " >= tail recursion: " + tailMax);
+		} else {
+			System.out.println("The function IS benefiting from tail recursion: non-tail recursion: " + nonTailMax + " < tail recursion: " + tailMax);
+		}
+	}
 
-    private static int maxIntNonTailRecursion(int n, FactorialCalculator factorialCalculator) {
-    	while(true) {
-    		try {
-    			factorialCalculator.factorialNonTailRecursion(n++);
-    		} catch (StackOverflowError e) {
-    			return n - 1;
-    		}
-    	}
-    }
+	private static int maxIntNonTailRecursion(int n, FactorialCalculator factorialCalculator) {
+		while(true) {
+			try {
+				factorialCalculator.factorialNonTailRecursion(n++);
+			} catch (StackOverflowError e) {
+				return n - 1;
+			}
+		}
+	}
 
-    private static int maxIntTailRecursion(int n, FactorialCalculator factorialCalculator) {
-    	while(true) {
-    		try {    			
+	private static int maxIntTailRecursion(int n, FactorialCalculator factorialCalculator) {
+		while(true) {
+			try {
 				factorialCalculator.factorialTailRecursion(n++);
-    		} catch (StackOverflowError e) {
-    			return n - 1;
-    		}
-    	}
-    }
+			} catch (StackOverflowError e) {
+				return n - 1;
+			}
+		}
+	}
 }
