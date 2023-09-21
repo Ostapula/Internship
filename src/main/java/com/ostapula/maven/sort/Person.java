dd1e464b69c28552227e1f7bfd6dd6da92d00c7f
ostapula <ostapnote2@gmail.com>
2023-09-21 18:44:44 +0200

    #9 Improved equals method.

--- src/main/java/com/ostapula/maven/sort/Person.java
+++ src/main/java/com/ostapula/maven/sort/Person.java
@@ -1,11 +1,13 @@
 package com.ostapula.maven.sort;
 
+import java.util.Objects;
+
 public class Person implements Comparable<Person>{
 
 	private final String name;
 	private final String surname;
 
-	protected Person(String name, String surname) {
+	public Person(String name, String surname) {
 		this.name = name;
 		this.surname = surname;
 	}
@@ -30,19 +32,17 @@ public class Person implements Comparable<Person>{
 		if (!(o instanceof Person))
 			return false;
 		Person p = (Person) o;
-		if (name == null || p.getName() == null || surname == null || p.getSurname() == null)
-			return false;
-		return name.equals(p.getName()) && surname.equals(p.getSurname());
+		return Objects.equals(name, p.getName()) && Objects.equals(surname, p.getSurname());
 	}
 
 	@Override
 	public int compareTo(Person p) {
 		if (this.equals(p))
 			return 0;
-		int result = compare(this.name, p.getName());
+		int result = compare(name, p.getName());
 		if (result != 0)
 			return result;
-		return compare(this.surname, p.getSurname());
+		return compare(surname, p.getSurname());
 	}
 
 	private int compare(String string1, String string2) {
